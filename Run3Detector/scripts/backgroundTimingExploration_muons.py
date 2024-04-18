22e850655f156ab5932b22f02670c4bb9fb41f74
FORMOSA admin account <formosa@lxplus914.cern.ch>
2024-04-18 13:14:55 +0200

    refining the script and adding one for the muons

--- Run3Detector/scripts/backgroundTimingExploration_muons.py
+++ Run3Detector/scripts/backgroundTimingExploration_muons.py
@@ -11,10 +11,10 @@ sys.path.append('/eos/experiment/formosa/steenis-general-functions')
 from general_functions import *
 sys.path.append(original_directory)
 #------------------------------------------------------------------------------------------------#
-filelist = ["/eos/experiment/formosa/commissioning/data/hadded_outputs/MilliQan_Run709_v35_throughGoingNoPanelsSkim.root:t"]
+filelist = ["/eos/experiment/formosa/commissioning/data/hadded_outputs/MilliQan_Run709_v35_throughGoingPanelsSkim.root:t"]
 variables = ["timeFit", "area", "chan", "layer", "timeFit_module_calibrated", "height"]
 
-outfile = ROOT.TFile("outputs/exploringWindowTiming.root", "RECREATE")
+outfile = ROOT.TFile("outputs/exploringWindowTiming_muons.root", "RECREATE")
 timeFitDiff = ROOT.TH1F("timeFitDifferences", "timeFitDifferences", 200,-100,100)
 shapeHist = ROOT.TH2F("shapeDist", "shapeDist", 200, 0, 1300, 100, 0, 200e3)
 wfTimeHist = ROOT.TH2F("wfTime", "wfTime", 200, 0, 1300, 100, 0, 200e3)
@@ -44,7 +44,7 @@ for data in uproot.iterate(filelist, variables, library='ak', step_size=100000):
     panelThresholdMaskBack = data['area']>panelThresholds[18]
 
     throughGoingMask = ak.any(((layer0)), axis=-1) & ak.any(((layer1)), axis=-1) & ak.any(((layer2)), axis=-1) & ak.any(((layer3)), axis=-1)
-    locationMask = throughGoingMask & ak.all(notChan16, axis=-1) & ak.all(notChan18, axis=-1) 
+    locationMask = throughGoingMask & ak.any(chan16, axis=-1) & ak.any(chan18, axis=-1) 
     data = data[locationMask] #We only want to consider throughGoingNonPanel events (event-level cut) 
     
     maxPerLayerMask = ((data['area']==ak.max(data['area'][data['layer']==0], axis=-1))) | ((data['area']==ak.max(data['area'][data['layer']==1], axis=-1))) | ((data['area']==ak.max(data['area'][data['layer']==2], axis=-1))) | ((data['area']==ak.max(data['area'][data['layer']==3], axis=-1)))
@@ -59,13 +59,13 @@ for data in uproot.iterate(filelist, variables, library='ak', step_size=100000):
     tempDiffs = ak.broadcast_arrays(tempDiffs, data['area'])[0]
     tempDiffs = ak.drop_none(tempDiffs)
     data['timeDiffs'] = tempDiffs
-    timeDiffMask = (ak.any(data['timeDiffs'] < -40, axis=-1)) & (ak.any(data['timeDiffs'] > -65, axis=-1))
-    data = data[timeDiffMask] #Removing events that don't have a layer0-layer3 timing difference in the negative peak.
+    '''timeDiffMask = (ak.any(data['timeDiffs'] < -40, axis=-1)) & (ak.any(data['timeDiffs'] > -65, axis=-1))
+    data = data[timeDiffMask] #Removing events that don't have a layer0-layer3 timing difference in the negative peak.'''
     
-    muonSeperatorMask = data['area'] > (20.31*data['height'])+4593.75
+    '''muonSeperatorMask = data['area'] > (20.31*data['height'])+4593.75
     nonSaturationMask = data['height'] < 1230
     nonMuonMask = muonSeperatorMask & nonSaturationMask
-    data = data[nonMuonMask] #Restrict to stuff outside the muon band (pulse-level cut)
+    data = data[nonMuonMask] #Restrict to stuff outside the muon band (pulse-level cut)'''
  
     timeDiffs03 = ak.flatten(data['timeDiffs'], axis=None)
     areas = ak.flatten(data['area'], axis=None)
@@ -94,12 +94,12 @@ shapeHist.GetXaxis().SetTitle("Height [mV]")
 shapeHist.GetYaxis().SetTitle("Area [pVs]")
 shapeHist.Write()
 
-wfTimeHist.SetTitle("Timing within Readout Window, Through-Going wo Panels")
+wfTimeHist.SetTitle("Timing within Readout Window, Through-Going w Panels")
 wfTimeHist.GetXaxis().SetTitle("timeFit_module_calibrated [ns]")
 wfTimeHist.GetYaxis().SetTitle("Area [pVs]")
 wfTimeHist.Write()
 
-wfTime_layer.SetTitle("Timing within Readout Window, Through-Going wo Panels")
+wfTime_layer.SetTitle("Timing within Readout Window, Through-Going w Panels")
 wfTime_layer.GetXaxis().SetTitle("timeFit_module_calibrated [ns]")
 wfTime_layer.GetYaxis().SetTitle("Layer")
 wfTime_layer.Write()
